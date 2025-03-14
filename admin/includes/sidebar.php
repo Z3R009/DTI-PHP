f3dccd36ba339c9a8bd763817fd8dc02e49d0519
James Zyrus <jamespama131@gmail.com>
2025-03-13 21:45:28 -0700

    sd

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,52 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+<li class="nav-item">
+<a class="navbar-brand ps-3" href="">
+    <img src="../img/DTI_w12.png" alt="Logo" style="height: 100px; width: auto; max-width: 100%; ">
+</a>
+</li>
+<li class="nav-item">
+<a class="nav-link " href="dashboard.php">
+    <i class="bi bi-grid"></i>
+    <span>Dashboard</span>
+</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+    <i class="bi bi-bar-chart"></i><span>Financial Management</span><i
+        class="bi bi-chevron-down ms-auto"></i>
+</a>
+<ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+    <li>
+        <a href="account_title.php">
+            <i class="bi bi-circle"></i><span>Account Titles</span>
+        </a>
+    </li>
+</ul>
+</li>
+
+<li class="nav-item">
+<a class="nav-link collapsed" href="reports_copy.php">
+    <i class="bi bi-journal-text"></i>
+    <span>Reports</span>
+</a>
+</li>
+
+<li class="nav-heading">User Management</li>
+
+<li class="nav-item">
+<a class="nav-link collapsed" href="manage_users.php">
+    <i class="bi bi-person"></i>
+    <span>Manage Users</span>
+</a>
+</li><!-- End Profile Page Nav -->
+
+
+
+</ul>
+
+</aside><!-- End Sidebar-->
