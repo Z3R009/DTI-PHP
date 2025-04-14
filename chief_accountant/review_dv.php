125a2a22957267c4f2f0fa2353ce14a4572bbaa5
james <jamespama131@gmail.com>
2025-04-14 09:12:42 +0800

    CA

--- chief_accountant/review_dv.php
+++ chief_accountant/review_dv.php
@@ -1,6 +1,6 @@
 <?php
 include '../DBConnection.php';
-include 'includes/header.php';
+
 
 if (!isset($_GET['id'])) {
     header('Location: pending_dv.php');
@@ -68,6 +68,54 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="../NiceAdmin/assets/img/favicon.png" rel="icon">
+    <link href="../NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="../NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/quill/quill.snow.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="../NiceAdmin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="../NiceAdmin/assets/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Updated: Apr 20 2024 with Bootstrap v5.3.3
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+<?php include 'Includes/header.php'; ?>
+<?php include 'Includes/sidebar.php'; ?>
+
  <main id="main" class="main">
 <div class="container-fluid">
     <div class="row">
@@ -178,4 +226,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </div>
 </div>
+
+</body>
+</html>
 
