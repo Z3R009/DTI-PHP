50d8fe3202795cc2a9c328e47ca15c25e0bf2112
John Abner <you@example.com>
2025-05-16 16:22:59 +0800

    REVIEW

--- chief_accountant/review_dv.php
+++ chief_accountant/review_dv.php
@@ -106,7 +106,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="../NiceAdmin/assets/img/favicon.png" rel="icon">
+    <link href="../book_keeper/img/dti_logo.png" rel="icon">
     <link href="../NiceAdmin/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
