b6e00dba6e155a6d04ec0e19526530bb749f7685
ZH4RK <ritzlarano@gmail.com>
2025-05-19 16:41:05 +0800

    payee - no update

--- book_keeper/payee.php
+++ book_keeper/payee.php
@@ -95,9 +95,6 @@ $select = mysqli_query($connection, "SELECT * FROM payee");
     <link href="../NiceAdmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="../NiceAdmin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
     <link href="../NiceAdmin/assets/css/style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
     <link rel="stylesheet" href="css/UACS.css">
     <link rel="stylesheet" href="css/table.css">
 
@@ -500,7 +497,6 @@ $select = mysqli_query($connection, "SELECT * FROM payee");
     <script src="../NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js"></script>
     <script src="../NiceAdmin/assets/vendor/tinymce/tinymce.min.js"></script>
     <script src="../NiceAdmin/assets/vendor/php-email-form/validate.js"></script>
-
 
 
     <!-- Direct search implementation -->
