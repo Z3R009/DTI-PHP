5b6dc764528e3ec297fceb78d8e937033f803acc
ZH4RK <ritzlarano@gmail.com>
2025-05-19 16:29:29 +0800

    ss

--- book_keeper/payee.php
+++ book_keeper/payee.php
@@ -95,6 +95,9 @@ $select = mysqli_query($connection, "SELECT * FROM payee");
     <link href="../NiceAdmin/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
     <link href="../NiceAdmin/assets/vendor/simple-datatables/style.css" rel="stylesheet">
     <link href="../NiceAdmin/assets/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
     <link rel="stylesheet" href="css/UACS.css">
     <link rel="stylesheet" href="css/table.css">
 
@@ -174,13 +177,14 @@ $select = mysqli_query($connection, "SELECT * FROM payee");
         .datatable-top .datatable-search {
             display: none !important;
         }
-        .modal-header{
-              background-color: #03045e;
+
+        .modal-header {
+            background-color: #03045e;
         }
-        .modal-title{
-              color: #fff;
+
+        .modal-title {
+            color: #fff;
         }
-        
     </style>
 
 </head>
@@ -496,6 +500,7 @@ $select = mysqli_query($connection, "SELECT * FROM payee");
     <script src="../NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js"></script>
     <script src="../NiceAdmin/assets/vendor/tinymce/tinymce.min.js"></script>
     <script src="../NiceAdmin/assets/vendor/php-email-form/validate.js"></script>
+
 
 
     <!-- Direct search implementation -->
